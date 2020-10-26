489d0d3039b57729b32e104a5bc28f912be26af5
bulltech18 <71047777+bulltech18@users.noreply.github.com>
2020-10-26 14:13:02 -0600

    Funciones

--- app/src/main/java/com/example/calculadora2/MainActivity.java
+++ app/src/main/java/com/example/calculadora2/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.calculadora2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0, btn_sum, btn_igual, btn_multi,
+    btn_div, btn_clear, btn_rest;
+    TextView pantalla;
+    String res, n1, n2, ide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
